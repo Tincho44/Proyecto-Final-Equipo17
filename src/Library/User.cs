6379cbef7dd1bb5894dadde251d1f8562092b8c8
Tincho44 <107601706+Tincho44@users.noreply.github.com>
2022-09-27 01:20:08 -0300

    Create User.cs

---
+++ src/Library/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Proyecto
+{
+    public class User
+    {
+    
+        private  string Name {get;}
+        private  string Surname {get;}
+        private  int ID {get;}
+        private  string Email {get;}
+        private  string Location {get; set;}
+        // private List<string> Usuarios {get; set;} 
+        // por el momento este atributo lo dejamos simplemente comentado, pero a futuro lo vamos a usar, no se si en esta clase u otra. 
+
+    }
+}
