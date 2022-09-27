0836edc754ec8a4b30ab9b083d3c66dd95edb419
Tincho44 <107601706+Tincho44@users.noreply.github.com>
2022-09-27 01:23:28 -0300

    Se crea la clase Employer

---
+++ src/Library/Employer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Proyecto
+{
+
+    public class Employer : User
+    {
+
+        public void CalificateEmployee(Employee employee)
+        {
+          // Califica a un Employee
+        }
+
+        public void ContactEmployee(Employee employee)
+        {
+            // Se contacta a un employee por sus servicios
+        }
+
+    }
+} 
