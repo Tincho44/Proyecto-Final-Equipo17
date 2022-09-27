af2b7543ebf347a8d032b8a6fef48601cc072860
FacuPintos <facupintos@hotmail>
2022-09-27 18:39:44 -0300

    Se agrega Clase Filter

---
+++ src/Library/Filter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Project{
+
+    public class Filter {
+
+        public List<Califications> califications;
+        public List<Categories> categories;
+        public List<User> users;
+
+
+        public void DeleteFilter(){
+
+        }
+
+        public void Search(){
+
+        }
+
+        public void SortByLocation(){
+
+        }
+
+        public void SortAlphabetically(){
+            
+        }
+
+    }
+
+
+
+
+
+}
