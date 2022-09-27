41ae51fb1dfb8d32a86ea518c2c4f40319e1eda5
Tincho44 <107601706+Tincho44@users.noreply.github.com>
2022-09-27 01:25:40 -0300

    Se crea la clase Administrator

---
+++ src/Library/Administrator.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace Proyecto
+{
+    public class Administrator : User
+    {
+
+        public void CreateCategories(Categories category) //da error por que falta crear la clase Categories
+        {
+            // Admin crea una categoria como dice la letra
+        }
+
+        public void RemoveService(Services service) // Lo mismo que categories, falta la clase Services
+        {
+            // Admin remueve un servicio
+        }
+
+        public void TalkToEmployee(Employee employee)
+        {
+            // El admin habla con el oferente para que no proponga servicios que no estan disponibles
+        }
+    }
+}
