1838d17dc63cb9f4146f7dba221cf1e149d18da0
FacuPintos <facupintos@hotmail>
2022-09-28 19:38:03 -0300

    Se agregan comentarios en la clase Categories

--- src/Library/Categories.cs
+++ src/Library/Categories.cs
@@ -10,17 +10,28 @@ namespace Project {
     public class Categories {
 
         private List<Categories> categoriesList;
-
+/// <summary>
+/// Constructor
+/// </summary>
         public Categories() {
 
             this.categoriesList = new List<Categories>();
         }
 
+/// <summary>
+/// Agrega una categoria dada a la lista de categorias
+/// </summary>
+/// <param name="cat"></param>
         public void AddCategories(Categories cat){
 
             this.categoriesList.Add(cat);                       
         }
 
+
+/// <summary>
+/// Modifica una categoria dada de la lista de categorias
+/// </summary>
+/// <param name="catMod"></param>
         public void ModifyCategorie(Categories catMod){
 
             for (int i = 0 ; i < this.categoriesList.Count ; i ++){
@@ -30,6 +41,11 @@ namespace Project {
             }
         }
 
+
+/// <summary>
+/// Elimina una categoria dada de la lista de categorias
+/// </summary>
+/// <param name="catDel"></param>
         public void DeleteCategorie(Categories catDel){
 
             for (int i = 0 ; i < this.categoriesList.Count ; i++){
