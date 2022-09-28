9bc7a3a8bf460b5ac7138c5ed9a39554f5ac8af6
FacuPintos <facupintos@hotmail>
2022-09-28 20:13:03 -0300

    Se agregan comentarios en clase Services

--- src/Library/Services.cs
+++ src/Library/Services.cs
@@ -9,17 +9,32 @@ namespace Project{
 /// </summary>
     public class Services{
 
+        /// <summary>
+        /// Lista de todos los servicios
+        /// </summary>
         public List<Services> services;
         private string name;
+        /// <summary>
+        /// Costo del servicio
+        /// </summary>
         public int cost;
         private Administrator admin;
 
-
+/// <summary>
+/// Agrega un servicio a la lista Servicios
+/// </summary>
+/// <param name="service"></param>
         public void AddService(Services service){
 
             this.services.Add(service);
         }
 
+/// <summary>
+/// Modifica un servicio de la lista Servicios
+/// </summary>
+/// <param name="service"></param>
+/// <param name="newCost"></param>
+/// <param name="newName"></param>
         public void ModifyService(Services service,int newCost, string newName){
 
             for (int i = 0 ; i < this.services.Count  ; i++){
@@ -30,11 +45,15 @@ namespace Project{
             }
         }
 
+/// <summary>
+/// Borra un servicio de la lista de servicios
+/// </summary>
+/// <param name="service"></param>
         public void DeleteService(Services service){
 
-            for (int i = 0 ; i < this.services.Count() ; i++){
+            for (int i = 0 ; i < this.services.Count ; i++){
 
-                if ( this.services[i] == services){
+                if ( this.services[i] == service){
 
                     this.services.RemoveAt(i);
 
@@ -42,12 +61,13 @@ namespace Project{
             }
         }
 
+/// <summary>
+/// Cambia el costo del costo de este objeto
+/// </summary>
+/// <param name="price"></param>
         public void PriceService(int price){
 
             this.cost = price;
         }
-
-
-
     }
 }
