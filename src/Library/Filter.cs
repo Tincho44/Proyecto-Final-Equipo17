edc6aa230af0e3d151eb9269bf970979d308a3f7
FacuPintos <facupintos@hotmail>
2022-09-28 20:28:18 -0300

    Se agregan comentarios en clase Filter

--- src/Library/Filter.cs
+++ src/Library/Filter.cs
@@ -10,27 +10,48 @@ namespace Project{
     public class Filter {
 
 
+        /// <summary>
+        /// Lista con las calificaciones
+        /// </summary>
         public List<Qualification> califications;
+        /// <summary>
+        /// Lista con las categorias
+        /// </summary>
         public List<Categories> categories;
+        /// <summary>
+        /// Lista con los usuarios
+        /// </summary>
         public List<User> users;
 
 
-        public void DeleteFilter(){
+        /// <summary>
+        /// Se encarga de eliminar un filtro dado.
+        /// </summary>
+        public void DeleteFilter(Filter filter){
 
         }
 
+        /// <summary>
+        /// Se encarga de realizar una busqueda por parametros dados
+        /// </summary>
         public void Search(){
 
         }
 
+        /// <summary>
+        /// Se encarga de ordenar por ubicacion mas cercana
+        /// </summary>
         public void SortByLocation(){
 
         }
 
+        /// <summary>
+        /// Se encarga de ordenar por orden alfabetico 
+        /// </summary>
         public void SortAlphabetically(){
             
         }
 
     }
-    
+
 }
