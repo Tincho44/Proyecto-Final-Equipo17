83a43c443f56e09dfb72816261d93ec0f66dbbbb
Tincho44 <martinharbuet@gmail.com>
2022-09-29 00:05:35 -0300

    errores

--- src/Library/User.cs
+++ src/Library/User.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 
 namespace Project
 {
-    public class User//superclase usuario de las que heredan administrador, empleado y empleador
+    public abstract class User//superclase usuario de las que heredan administrador, empleado y empleador
     {
     
-        private  string Name {get;}
-        private  string Surname {get;}
-        private  int ID {get;}
-        private  string Email {get;}
-        private  string Location {get; set;}
+        private  string name; 
+        private  string surname;
+        private  int identity;
+        private  string email;
+        private  string Location;
+
         // private List<string> Usuarios {get; set;} 
         // por el momento este atributo lo dejamos simplemente comentado, pero a futuro lo vamos a usar, no se si en esta clase u otra. 
 
     }
+
+    public User (string name,string surname,string location)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.qualification = qualification;
+            this.location = location;
+            this.reputation = reputation;
+        }
+
+    public string name
+        {
+            get { return name; }
+        }
+
+    public string GetName(string name)
+        {
+            get {return name} 
+        }    
 }
